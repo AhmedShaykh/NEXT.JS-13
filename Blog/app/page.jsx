0ec3a10f6cf325d8e818f6757191b469f85b31ad
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-01-23 21:37:46 +0500

    Git Init

--- Blog/app/page.jsx
+++ Blog/app/page.jsx
@@ -1,10 +1,31 @@
 import styles from './page.module.css';
+import { getSortedPostsData } from '../lib/posts.js';
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+};
+
+export default function Home({ allPostsData }) {
   return (
     <main className={styles.main}>
       <div>
-        <h1>Blog APP</h1>
+        <h1 >Blog</h1>
+        <ul>
+          {allPostsData.map(({ id, date, title }) => (
+            <li key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))};
+        </ul>
       </div>
     </main>
   )
