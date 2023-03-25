f7201862a6b4c33377fe81933caca28eaa193534
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-03-25 16:58:11 +0500

    Add API's

--- app/invokeapi/(api)/fetch_client/page.tsx
+++ app/invokeapi/(api)/fetch_client/page.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import Link from 'next/link';
 import styles from '../../../page.module.css';
 
 const page = () => {
     return (
         <div className={styles.main}>
-            <h3></h3>
+            <h3>Fetching In Client</h3>
+
+            <Link href="/invokeapi">
+                <p
+                    style={{ marginTop: "1.5rem" }}
+                    className={styles.link}
+                >
+                    Back To Home
+                </p>
+            </Link>
         </div>
     )
 };
