817ac0821279d63a3e3d891b47def0d806a0af22
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-03-25 16:53:18 +0500

    Add API's

---
+++ app/invokeapi/(api)/fetch_client/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from '../../../page.module.css';
+
+const page = () => {
+    return (
+        <div className={styles.main}>
+            <h3></h3>
+        </div>
+    )
+};
+
+export default page;
--- app/invokeapi/page.tsx
+++ app/invokeapi/page.tsx
@@ -1,13 +1,25 @@
 import { FC } from 'react';
+import Link from 'next/link';
 import styles from '../page.module.css';
 
 const Posts: FC = () => {
-
     return (
         <div className={styles.main}>
             <h3>
                 Invoking API's
             </h3>
+
+            <br />
+
+            <Link href="/invokeapi/server_static">
+                <h5 className={styles.heading}>1) Fetching Data Server Static</h5>
+            </Link>
+            <Link href="/invokeapi/server_dynamic">
+                <h5 className={styles.heading}>2) Fetching Data Server Dynamic</h5>
+            </Link>
+            <Link href="/invokeapi/fetch_client">
+                <h5 className={styles.heading}>3) Fetching Data Client</h5>
+            </Link>
         </div>
     )
 };
