5de85aec256cb152a623d2e4b2febf5affc6c9e5
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-04-14 23:48:50 +0500

    Remove React

--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -5,15 +5,15 @@ const Navbar = () => {
     return (
         <div className={styles.navbar}>
             <div className={styles.links}>
+
                 <Link href="/" className={styles.link}>
                     Home
                 </Link>
+
                 <Link href="/invokeapi" className={styles.link}>
                     API
                 </Link>
-                <Link href="/react" className={styles.link}>
-                    React
-                </Link>
+
             </div>
         </div>
     )
